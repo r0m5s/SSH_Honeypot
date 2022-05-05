65058f7b35ad8aa1c7435fb64deef811f32276a6
r0m5s <rsilva01wk@gmail.com>
2022-05-05 11:10:28 +0100

    General Changes

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -73,10 +73,10 @@ class BasicSshHoneypot(paramiko.ServerInterface):
             logging.info('new client credentials ({}): username: {}, password: {}'.format(self.client_ip, username, password))
 
             #Redirection to Honeypot System
-            local_ip_address = "192.168.122.99"
+            local_ip_address = "192.168.0.1"
             local_port = 22
-            local_username = "rm"
-            local_password = "rm"
+            local_username = "client"
+            local_password = "client"
 
             return paramiko.AUTH_SUCCESSFUL
         else:
@@ -84,10 +84,10 @@ class BasicSshHoneypot(paramiko.ServerInterface):
             logging.info('new client credentials ({}): username: {}, password: {}'.format(self.client_ip, username, password))
 
             #Redirection to Protected Client
-            local_ip_address = "127.0.0.1"
+            local_ip_address = "192.168.2.2"
             local_port = 22
-            local_username = "romas"
-            local_password = "zezinho"
+            local_username = "client"
+            local_password = "client"
 
             return paramiko.AUTH_SUCCESSFUL
 
